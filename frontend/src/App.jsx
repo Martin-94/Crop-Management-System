10748bc22203f67b0ad1f110fe999e5430f17096
Nikita Gordhan <tris040610@gmail.com>
2026-07-03 09:30:34 +0200

    Final Regsitration Screen code completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
-import MainScreen from './pages/MainScreen'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainScreen from "./pages/MainScreen";
+import RegistrationScreen from "./pages/RegistrationScreen";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainScreen />} />
-    </Routes>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainScreen />} />
+        <Route path="/register" element={<RegistrationScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
